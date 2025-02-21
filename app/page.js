ca6b5866e12f1b6bed44e9801d8fbe88c4f5b923
firaterror <141954778+firaterror@users.noreply.github.com>
2025-02-21 06:14:19 +0000

    found a subdomain takeover updating to report it

--- app/page.js
+++ app/page.js
@@ -5,9 +5,8 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="w-full h-full">
-      <Options className="relative min-h-screen"/>
-      <Hero className="min-h-screen"/>
+    <div>
+      <h1>ALEXANDER GONZALEZ LETS GERMAN USE HIS MANSCAPED</h1>
     </div>
   );
 }
