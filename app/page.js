de54565a377407206ad094a778261f3c55620cf2
firaterror <firatwells@icloud.com>
2025-04-07 22:20:04 -0400

    reverted back

--- app/page.js
+++ app/page.js
@@ -5,8 +5,9 @@ import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>
-      <h1>ALEXANDER GONZALEZ LETS GERMAN USE HIS MANSCAPED</h1>
+    <div className="w-full h-full">
+      <Options className="relative min-h-screen"/>
+      <Hero className="min-h-screen"/>
     </div>
   );
 }
